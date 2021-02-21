92b9e6b696e1cd4957692af37e0ad71afa66948e
Bryan Dijkhuizen <dijkhuizenb@outlook.com>
2021-02-21 14:23:30 +0100

    new function

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Grabs a cookie by name.
 # Installation
 
 `npm i cookiegrabber --save`
+
+Then...
+
+...
+import {cookiegrabber} from 'cookiegrabber';
+
+cookiegrabber({name: cookie_name});
+...
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-function cookiegrabber(options){
+function getCookie(options){
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${options.name}=`);
     if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+function setCookie(options){
+    var d = new Date;
+    d.setTime(d.getTime() + 24*60*60*1000*options.days);
+    document.cookie = options.name + "=" + options.value + ";path=/;expires=" + d.toGMTString();
 }
 
 module.exports.cookiegrabber = cookiegrabber;
